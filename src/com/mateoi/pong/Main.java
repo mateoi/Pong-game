49665da2feb272c042d28295e19e9a03f8824027
mateo i <mateo i>
2016-11-25 15:05:31 +0000

    Added statistics measurements

--- src/com/mateoi/pong/Main.java
+++ src/com/mateoi/pong/Main.java
@@ -4,12 +4,16 @@ import javafx.scene.input.KeyCode;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Player leftPlayer = new HumanPlayer(KeyCode.W, KeyCode.S);
-        Player rightPlayer = new HumanPlayer(KeyCode.UP, KeyCode.DOWN);
-        PongGame game = new PongGame(500, 300);
-        PongFXApp.setGame(game);
-        PongFXApp.setPlayers(leftPlayer, rightPlayer);
-        PongFXApp.launch(PongFXApp.class);
-    }
+	public static void main(String[] args) {
+		final Player leftPlayer = new HumanPlayer(KeyCode.W, KeyCode.S);
+		final Player rightPlayer = new HumanPlayer(KeyCode.UP, KeyCode.DOWN);
+		final PongGame game = new PongGame(500, 300);
+		PongFXApp.setGame(game);
+		PongFXApp.setPlayers(leftPlayer, rightPlayer);
+		PongFXApp.launch(PongFXApp.class);
+
+		System.out.println("Longest Rally: " + game.getLongestRally());
+		System.out.println("Average Rally: " + game.getAverageRally());
+		System.out.println("Dead balls: " + game.getDeadBalls());
+	}
 }
